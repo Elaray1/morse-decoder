633f4b1afca9604f055a3ebe85999e322dcb0ed8
Egor <5642452@mail.ru>
2019-09-22 22:44:16 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let length = expr.length;
+  let decodedStr = "";
+  let n = length / 10;
+  let startSubstring = 0;
+  for (let i = 0; i < n; i++, startSubstring += 10) {
+    let symbol = "";
+    let codedStr = expr.substr(startSubstring, 10);
+    if (codedStr[0] == "*") {
+      decodedStr += " ";
+      continue;
+    }
+    let j = 0;
+    while (codedStr[j] == "0") j++;
+    j++;
+    for (j; j < 10; j += 2) {
+      if (codedStr[j] == 0) {
+        symbol += ".";
+      } else {
+        symbol += "-";
+      }
+    }
+    decodedStr += MORSE_TABLE[symbol];
+  }
+  return decodedStr;
 }
 
 module.exports = {
